06ceb9044c59fa627f95acc85988090142b7d2b1
ZGorringe <zacgorringe@gmail.com>
2014-09-16 12:44:31 -0600

    updated to array problem

--- callbackPractice.js
+++ callbackPractice.js
@@ -8,29 +8,77 @@ first(names, function(firstName){
   console.log('The first name in names is ', firstName)
 });
 
+var getName = function(cb){
+  var thisName = names[0];
+  cb(thisName);  
+}
+
 last(names, function(lastName){
   console.log('The last name in names is ', lastName);
 });
+
+var getName = function(cb){
+  var thisName = names[names.length-1];
+  cb(thisName);  
+}c
 
 multiply(num1, num2, function(answer){
   console.log('The answer is ', answer);
 })
 
+var math = function(cb){
+  var theAnswer = num1 * num2;
+  cb(theAnswer);
+}
+
 contains(names, 'Colt', function(yes){
   if(yes === true){
-    'Colt is in the array';
+    console.log('Colt is in the array');
   } else {
-    'Colt is not in the list';
+    console.log('Colt is not in the list');
   }
 });
+
+var findName = function(cb){
+  for(var i = 0; i < names.length; i++){
+    if(names[i] === 'Colt'){
+      var yes = true;
+    }
+    else {
+      var yes = false;
+    }
+  }
+  cb(yes);
+}
+
 
 uniq(names, function(uniqArr){
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
+var sortedNames = names.sort();
+
+var removeName = function(cb){
+  for(var i = 0; i < sortedNames.length; i++){
+    if(sortedNames[i + 1] === sortedNames[i]){
+      sortedNames.splice(sortedNames[i]);
+    }
+  }
+  cb(sortedNames);
+}
+
 each(names, function(item, indice){
   console.log('The item in the ' + indice + 'position is ' + item)
 });
+
+var findName = function(cb){
+  var index = names.indexOf
+  for(var i = 0; i < names.length; i++){
+    var index = names.indexOf[i];
+    var name = names[i];
+  }
+  cb(name, index);
+}
 
 var users = [
   {
